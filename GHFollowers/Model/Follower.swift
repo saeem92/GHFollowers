43e527a0816081bfdd3e96d8919a937f5683a9dc
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-11-12 19:47:53 +0530

    changes

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by saeem  on 12/11/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GHFollowers/Model/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  GHFollowers
+//
+//  Created by saeem  on 12/11/22.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+}
